2061be5b4dcac80e1b75a03608bfda3a04e44ceb
chcv1234 <chcv1234@gmail.com>
2019-06-09 17:06:00 +0800

    schedule set
    loading the schedule data

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,52 @@
 //
 // TODO(you): Add the JavaScript necessary to complete your final project.
 //
+(() => {
+    async function onSubmit(event){
+        event.preventDefault();
+        const response = await fetch("/api", {method: "GET"});
+        const json = await response.json();
+
+        const cnt = 0 ;
+
+        console.log(json);
+
+        const date = document.querySelector('#test1');
+        date.innerHTML = json[0].date ;
+
+    }
+
+    async function sheculeGET(event){
+        const response = await fetch("/api", {method: "GET"});
+        const json = await response.json();
+
+        console.log(json);
+
+        //const day = '#' + 'test' + json[0].day ;
+
+        const date = document.querySelectorAll('.flex-item');
+
+        for(let i=0 ; i < 7 ; i++)
+        {
+            if(date[i].id === ('test' + json[0].day) )
+            {
+                console.log('testyes');
+                date[i].innerHTML = json[0].date ;
+                cnt = i ;
+                break ;
+            }
+        }
+
+        for(let i=cnt+1 ; i < cnt + json.length  ; i++ )
+        {
+            date[i].innerHTML = json[i-cnt].date ;
+        }
+
+    }
+
+    const send = document.querySelector('#test');
+    send.addEventListener('click', onSubmit);
+
+   sheculeGET();
+
+})();
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const googleSheets = require('gsa-sheets');
 
 // TODO(you): Update the contents of privateSettings accordingly, as you did
 // in HW5, then uncomment this line.
-// const key = require('./privateSettings.json');
+ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet, as you did in HW5, then uncomment these lines.
-// const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
-// const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
+ const SPREADSHEET_ID = '1YoE1cOMmCFy_gLXIydN_R0aOG54nRMg-1vcawKlEgeo';
+ const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -17,7 +17,32 @@ const jsonParser = bodyParser.json();
 app.use(express.static('public'));
 
 // TODO(you): Add at least 1 GET route and 1 POST route.
+async function onGet(req, res) {
+    const result = await sheet.getRows();
+    const rows = result.rows;
+    console.log(rows);
 
+    var array = [] ;
+
+    // TODO(you): Finish onGet.
+    for(let i=1 ; i < rows.length; i++)
+    {
+        let line = {} ;
+
+        for(let j=0 ; j< rows[0].length; j++)
+        {
+            line[rows[0][j]] = rows[i][j] ;
+        }
+
+        array.push(line);
+
+        console.log(array);
+        console.log(line);
+    }
+    //res.json( { status: 'unimplemented!!!'} );
+    res.json(array);
+}
+app.get('/api', onGet);
 // Please don't change this; this is needed to deploy on Heroku.
 const port = process.env.PORT || 3000;
 
